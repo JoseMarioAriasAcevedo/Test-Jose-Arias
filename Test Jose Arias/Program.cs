3d3393a196a7e13142fc82ec1b37f7d6876e0a69
JoseMarioAriasAcevedo <ariasjosem@javeriana.edu.co>
2025-03-19 17:12:58 -0500

    Agregar archivos de proyecto.

---
+++ Test Jose Arias/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Test_1_Jose_Arias
+{
+    internal class Class
+    {
+        static void Main(String[] args)
+        {
+            //Solution to problem 1A
+            Object obj = new Object();
+            Console.WriteLine("Enter your name");
+            String name = Console.ReadLine();
+            Console.WriteLine(obj.Hello(name));
+
+        }
+    }
+    public class Object
+    {
+        public string a = "Hello ";
+
+        public string Hello(string name)
+        {
+            return a + name;
+        }
+    }
+}
+
+// Solution 1B
+// To make "name" inmutable you will need to write readonly when creating the atribute
